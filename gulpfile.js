16bb6a67a5bdcefcdd48d17e118c51f2a658dc0a
dimidroll450 <dmitriykvas@outlook.com>
2021-04-09 14:36:18 +0300

    upgrade gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,74 @@
 'use strict'
+
+// === Settings ===
+const settings = {
+  scripts: false,
+  styles: true,
+  copy: false,
+}
+
+ // === Imports ===
 const { src, dest, series, watch } = require('gulp');
 
+// === Styles ===
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
+// === BrowserSync ===
+const browserSync = require('browser-sync').create();
+
 sass.compiler = require('node-sass');
 
 const paths = {
-  scss: './src/scss/*.{scss, sass}',
-  copy: './src/*.html',
+  src: {
+    scss: './src/scss/**/*.scss',
+    images: './src/images/*',
+    html: './src/*.html',
+  },
+  dist: {
+    main: './dist',
+  },
 }
  
-const scssTask = () => (
-  src(paths.scss)
-    .pipe(sass())
+const buildCSS = (done) => {
+  if (!settings.styles) {
+    return done();
+  }
+
+  return src('./src/scss/app.scss')
+    .pipe(sass({
+      outputStyle: 'expanded',
+    }))
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
-    .pipe(dest('./dist/css'))
-);
+    .pipe(dest('./src/css'))
+    .pipe(browserSync.stream())
+};
 
-const copyFiles = () => src(paths.copy)
-  .pipe(dest('./dist'))
+const copyFiles = (done) => {
+  if (!settings.html) {
+    return done();
+  }
 
-const watchTask = () => {
-  watch(paths.scss, ['sass']);
+  return src(paths.src.copy)
+    .pipe(dest(paths.dist.main));
+};
+
+const watchers = () => {
+  browserSync.init({
+    server: {
+      baseDir: './src',
+    },
+  });
+
+  watch(paths.src.scss, buildCSS);
+  watch(paths.src.html).on('change', browserSync.reload);
 }
 
 exports.default = series(
-  scssTask,
+  buildCSS,
   copyFiles,
-)
+);
+
+exports.watch = watchers;
